c878855973cbe017575a07a821984440dc5b174a
Gaetano Giunta <gg@ez.no>
2009-01-20 13:34:14 +0000

    - add license, info files

---
+++ ezinfo.php
@@ -0,0 +1,14 @@
+<?php
+class xsendfilehandlerInfo
+{
+    static function info()
+    {
+        return array(
+            'Name' => "X-SendFile binary file download handler",
+            'Version' => "0.2",
+            'Copyright' => "Copyright (C) 2008,2009 K. Coomans",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>
